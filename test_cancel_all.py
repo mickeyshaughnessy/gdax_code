023bde41f19b2a364149ad0e4c84fb4ee092d4cf
Mickey Shaughnessy <mickeyshaughnessy@NSA-WebCache.local>
2017-06-19 21:57:18 -0600

    more testing

--- test_cancel_all.py
+++ test_cancel_all.py
@@ -6,6 +6,7 @@ from market_maker import cancel_all, make_limit
 from test_limit import get_safe
 import pprint
 import json
+import time
 
 if __name__ == "__main__":
     print 'This may spend real money -- proceed? (y/n)'
@@ -15,14 +16,13 @@ if __name__ == "__main__":
         # make a sell order at 2x the bid
         safe = get_safe()
         resp = make_limit(side='sell', size=0.1, price=safe, auth=auth)
-        print resp
-        raw_input()
         r = requests.get(base_url + '/orders/', auth=auth)
+        print 'Orders:'
         print pprint.pprint(r.json())
         resp = cancel_all(auth=auth)
-        print resp
-        raw_input()
+        time.sleep(5)
         r = requests.get(base_url + '/orders/', auth=auth)
+        print 'Orders:'
         print pprint.pprint(r.json())
     else:
         sys.exit()
