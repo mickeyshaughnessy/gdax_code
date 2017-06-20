9bc4ee51468c55d93b3ee142e3b7cec0e501ebf4
Mickey Shaughnessy <mickeyshaughnessy@NSA-WebCache.local>
2017-06-20 11:07:14 -0600

    initial market maker logic

--- market_maker.py
+++ market_maker.py
@@ -3,9 +3,10 @@ import requests
 from config import *
 import json
 
-def get_position(auth=None):
+def get_position(product='ETH', auth=None):
     r = requests.get(base_url + '/position/', auth=auth) 
-    return r.json() 
+    resp = r.json()
+    return float(resp['accounts'][product].get(hold))
 
 def cancel_all(auth=None):
     r = requests.delete(base_url + '/orders', auth=auth)
@@ -20,17 +21,31 @@ def make_limit(side='buy', size=0.0, price=0.0, product='ETH-USD', auth=None):
     r = requests.post(base_url + '/orders', data=order, auth=auth)
     return r.json()
 
+def get_bid_ask(product='ETH-USD'):
+    r = requests.get(base_url + '/products/%s/book' % product)
+    resp = r.json()
+    bid = float(resp['bids'][0][0])
+    ask = float(resp['asks'][0][0])
+    return bid, ask
+    
+
 def make_market():
+    auth = GdaxAuth(key, secret, passphrase)
     while True:
         sleep(1)
-        pos = get_position()
-        bid_ask = get_bid_ask()
-        if abs(pos) < 500: #USD
-            # put in two sided limit order
-			pass
+        eth_pos = get_position(product='ETH', auth=auth)
+        usd_pos = get_position(product='USD', auth=auth)
+        bid, ask = get_bid_ask()
+        print 'ETH_pos = %s, USD_pos = %s, bid/ask = %s - %s' % (eth_pos, usd_pos, bid, ask)
+        if eth_pos < 0.1 and usd_pos < 50: # ETH / USD
+            make_limit(side='buy', size = 0.01, price=0.99*bid) 
+            make_limit(side='sell', size = 0.01, price=1.01*bid) 
+        elif eth_pos < 0.1:
+            make_limit(side='buy', size = 0.01, price=0.99*bid)
+        elif usd_pos < 50:
+            make_limit(side='sell', size = 0.01, price=1.01*bid)
         else:
             cancel_all()
-
 
 if __name__ == "__main__":
     make_market() 
--- test_position.py
+++ test_position.py
@@ -2,10 +2,10 @@ import requests
 from config import *
 from gdax_auth import GdaxAuth
 import pprint
-from market_maker import get_position
 
 if __name__ == "__main__":
     auth = GdaxAuth(key, secret, passphrase)
-    resp = get_position(auth)
+    r = requests.get(base_url + '/position/', auth=auth)
+    resp = r.json()
     print '---- Position ----'
     pprint.pprint(resp)
