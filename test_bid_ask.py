5377fb783f26a74ebed5253c0fa0b0c4bf25ee08
Mickey Shaughnessy <mickeyshaughnessy@NSA-WebCache.local>
2017-06-20 11:07:36 -0600

    add test bid ask

---
+++ test_bid_ask.py
@@ -0,0 +1,9 @@
+import requests
+from config import *
+from gdax_auth import GdaxAuth
+import pprint
+from market_maker import get_bid_ask
+
+if __name__ == "__main__":
+    resp = get_bid_ask() 
+    print 'bid: %s, ask: %s' % (resp[0], resp[1])
